3f56ae08bd7227152a1a3183a4fd716899466d23
Your Name <your_email@example.com>
2026-01-19 21:34:47 +0100

    🎨 style: Switch to catppuccin colorscheme

--- init.lua
+++ init.lua
@@ -2,4 +2,17 @@ require("config.lazy")
 require("config.keymaps")
 require("config.lsp")
 vim.o.background = "dark" -- or "light" for light mode
-vim.cmd([[colorscheme gruvbox]])
+vim.cmd([[colorscheme catppuccin-mocha]])
+
+-- local backupdir = vim.fn.expand("~/.vim/backup/")
+-- local undodir = vim.fn.expand("~/.vim/undo/")
+-- local swapdir = vim.fn.expand("~/.vim/swap/")
+-- if vim.fn.isdirectory(undodir) == 0 then
+--   vim.fn.mkdir(undodir, "p")
+-- end
+-- if vim.fn.isdirectory(swapdir) == 0 then
+--   vim.fn.mkdir(swapdir, "p")
+-- end
+-- if vim.fn.isdirectory(backupdir) == 0 then
+--   vim.fn.mkdir(backupdir, "p")
+-- end
--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,8 +1,1 @@
---return { "catppuccin/nvim", 
---  name = "catppuccin", 
---  priority = 1000,
---  opts= {transparent_background = true}
---}
-return { "ellisonleao/gruvbox.nvim", priority = 1000 , config = true, opts = {
-   transparent_mode = true,
-} }
+return { "catppuccin/nvim", name = "catppuccin", priority = 1000 }
