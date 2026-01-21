a8ec5a398faba4bc2dfe4ff6e68f1a679c565d64
Your Name <your_email@example.com>
2026-01-21 23:48:28 +0100

    🔧 refactor: Update Telescope keymaps and file ignore patterns

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -58,8 +58,16 @@ return { -- Fuzzy Finder (files, lsp, etc)
           },
         },
       },
-      pickers = { find_files = { hidden = true } },
-      live_grep = { additional_args = { "--hidden" } },
+      pickers = {
+        find_files = {
+          file_ignore_patterns = { 'node_modules', '.git', '.venv', '.stfolder' },
+          hidden = true
+        }
+      },
+      live_grep = {
+        file_ignore_patterns = { 'node_modules', '.git', '.venv', '.stfolder' },
+        additional_args = { "--hidden" }
+      },
     })
 
     -- Enable Telescope extensions if they are installed
@@ -68,18 +76,18 @@ return { -- Fuzzy Finder (files, lsp, etc)
 
     -- See `:help telescope.builtin`
     local builtin = require("telescope.builtin")
-    vim.keymap.set("n", "th", builtin.help_tags, { desc = "[S]earch [H]elp", noremap = true, silent = true })
-    vim.keymap.set("n", "tk", builtin.keymaps, { desc = "[S]earch [K]eymaps", noremap = true, silent = true })
-    vim.keymap.set("n", "tf", builtin.find_files, { desc = "[S]earch [F]iles" })
-    vim.keymap.set("n", "tw", builtin.grep_string, { desc = "[S]earch current [W]ord" })
-    vim.keymap.set("n", "tg", builtin.live_grep, { desc = "[S]earch by [G]rep" })
-    vim.keymap.set("n", "td", builtin.diagnostics, { desc = "[S]earch [D]iagnostics" })
-    vim.keymap.set("n", "tr", builtin.resume, { desc = "[S]earch [R]esume" })
-    vim.keymap.set("n", "t.", builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
-    vim.keymap.set("n", "tb", builtin.buffers, { desc = "[ ] Find existing buffers" })
-    vim.keymap.set("n", "ts", builtin.lsp_document_symbols, { desc = "[S]earch [s]ymbols in File" })
-    vim.keymap.set("n", "tS", builtin.lsp_workspace_symbols, { desc = "[S]earch [S]ymbols in Workspace" })
-    vim.keymap.set("n", "t/", function()
+    vim.keymap.set("n", "<leader>se", builtin.help_tags, { desc = "[S]earch H[e]lp", noremap = true, silent = true })
+    vim.keymap.set("n", "<leader>sk", builtin.keymaps, { desc = "[S]earch [K]eymaps", noremap = true, silent = true })
+    vim.keymap.set("n", "<leader>sf", builtin.find_files, { desc = "[S]earch [F]iles" })
+    vim.keymap.set("n", "<leader>sc", builtin.grep_string, { desc = "[s]earch [c]urrent word" })
+    vim.keymap.set("n", "<leader>sg", builtin.live_grep, { desc = "[S]earch by [G]rep" })
+    vim.keymap.set("n", "<leader>sd", builtin.diagnostics, { desc = "[S]earch [D]iagnostics" })
+    vim.keymap.set("n", "<leader>sr", builtin.resume, { desc = "[S]earch [R]esume" })
+    vim.keymap.set("n", "<leader>s.", builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
+    vim.keymap.set("n", "<leader><leader>", builtin.buffers, { desc = "[ ] Find existing buffers" })
+    vim.keymap.set("n", "<leader>ss", builtin.lsp_document_symbols, { desc = "[S]earch [s]ymbols in File" })
+    vim.keymap.set("n", "<leader>sS", builtin.lsp_workspace_symbols, { desc = "[S]earch [S]ymbols in Workspace" })
+    vim.keymap.set("n", "<leader>s/", function()
       -- You can pass additional configuration to Telescope to change the theme, layout, etc.
       builtin.current_buffer_fuzzy_find(require("telescope.themes").get_dropdown({
         winblend = 10,
@@ -89,7 +97,7 @@ return { -- Fuzzy Finder (files, lsp, etc)
 
     -- It's also possible to pass additional configuration options.
     --  See `:help telescope.builtin.live_grep()` for information about particular keys
-    vim.keymap.set("n", "tl", function()
+    vim.keymap.set("n", "<leader>sl", function()
       builtin.live_grep({
         grep_open_files = true,
         prompt_title = "Live Grep in Open Files",
@@ -97,8 +105,8 @@ return { -- Fuzzy Finder (files, lsp, etc)
     end, { desc = "[S]earch [/] in Open Files" })
 
     -- Shortcut for searching your Neovim configuration files
-    vim.keymap.set("n", "tv", function()
+    vim.keymap.set("n", "<leader>sn", function()
       builtin.find_files({ cwd = vim.fn.stdpath("config") })
-    end, { desc = "Search Neo[V]im files" })
+    end, { desc = "[s]earch [n]eoVim files" })
   end,
 }
