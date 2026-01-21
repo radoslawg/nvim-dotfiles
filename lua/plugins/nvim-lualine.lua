b38fb949b53ad73ef2eabbfe7cc830eacbdea677
Your Name <your_email@example.com>
2026-01-21 14:21:55 +0100

    feat(statusline): ✨ replace ministatusline with nvim-lualine

--- lua/plugins/nvim-lualine.lua
+++ lua/plugins/nvim-lualine.lua
@@ -1,8 +1,9 @@
-return { 
+return {
 	'nvim-lualine/lualine.nvim',
+	version = '*', -- latest stable release
 	dependencies = { 'nvim-tree/nvim-web-devicons' },
 	opts = {
-		theme = 'gruvbox',
+		theme = 'gruvbox-material',
 		icons_enabled = true,
 	},
 	Lazy = false,
