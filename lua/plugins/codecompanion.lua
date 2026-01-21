85224f236b2cb7dd81cdf8cbac92f2f9f969a36d
Your Name <your_email@example.com>
2026-01-21 23:48:21 +0100

    ✨ feat: Add codecompanion.nvim and mcphub.nvim plugins

---
+++ lua/plugins/codecompanion.lua
@@ -0,0 +1,10 @@
+return {
+  "olimorris/codecompanion.nvim",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "nvim-treesitter/nvim-treesitter",
+  },
+  opts = {
+    -- NOTE: The log_level is in `opts.opts`
+  },
+}
