402fd0579f2502843f6c99b6b495685dc1681ff8
Your Name <your_email@example.com>
2026-01-22 15:13:41 +0100

    :art: style: Configure lualine with gruvbox-material theme and icons

--- lua/plugins/nvim-lualine.lua
+++ lua/plugins/nvim-lualine.lua
@@ -1,10 +1,10 @@
 return {
-	'nvim-lualine/lualine.nvim',
-	version = '*', -- latest stable release
-	dependencies = { 'nvim-tree/nvim-web-devicons' },
-	opts = {
-		theme = 'gruvbox-material',
-		icons_enabled = true,
-	},
-	Lazy = false,
+  'nvim-lualine/lualine.nvim',
+  version = '*', -- latest stable release
+  dependencies = { 'nvim-tree/nvim-web-devicons' },
+  opts = {
+    theme = 'gruvbox-material',
+    icons_enabled = true,
+  },
+  Lazy = false,
 }
