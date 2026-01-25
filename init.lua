05f03c06d5802d12debe7bc3a73e56e6e9d918fc
Radek <radekg@debian.debian.grzanka.org>
2026-01-25 22:20:28 +0100

    Now for real, autoupdate?

--- init.lua
+++ init.lua
@@ -22,12 +22,11 @@ vim.api.nvim_create_autocmd("User", {
   pattern = "VeryLazy",
   callback = function()
     local checker = require("lazy.manage.checker")
-    local lazy = require("lazy")
     checker.check()
-    local updates = #checker.updated
-    vim.api.nvim_echo({ { "Updates: " .. updates } }, true, {})
-    if updates > 0 then
-      vim.api.nvim_echo({ { "Jestem" }, }, true, {})
+    local status = require("lazy.status")
+    if status.has_updates then
+      --      vim.api.nvim_echo({ { "Status.has_updates" } }, true, {})
+      local lazy = require("lazy")
       lazy.sync({ show = false }) -- Sync plugins without showing the UI
     end
   end,
