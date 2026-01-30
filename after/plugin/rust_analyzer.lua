b3ac2a417315c59ca30ee553cab21e0e26ee528c
Radoslaw Grzanka <radoslawg@gmail.com>
2026-01-30 22:00:28 +0100

    :sparkles: feat: add specific keymaps for rust_analyzer

---
+++ after/plugin/rust_analyzer.lua
@@ -0,0 +1,32 @@
+-- 1. Define the autocommand to handle keybindings
+-- We create a group to prevent duplicating the autocmd if you reload the file
+local augroup = vim.api.nvim_create_augroup("RustAnalyzerKeys", { clear = true })
+
+vim.api.nvim_create_autocmd("LspAttach", {
+  group = augroup,
+  callback = function(args)
+    -- Get the client that just attached to the buffer
+    local client = vim.lsp.get_client_by_id(args.data.client_id)
+
+    -- Ensure we only apply these mappings when 'rust_analyzer' is the one attaching
+    if client and client.name == "rust_analyzer" then
+      -- Set the keymap only for the current buffer
+      vim.keymap.set("n", "<leader>cr", "<cmd>Crun<CR>", {
+        buffer = args.buf,
+        desc = "Cargo [r]un",
+      })
+    end
+    vim.keymap.set("n", "<leader>cD", "<cmd>Cdoc --open<CR>", {
+      buffer = args.buf,
+      desc = "Cargo open [D]ocs",
+    })
+    vim.keymap.set("n", "<leader>cb", "<cmd>Cbuild<CR>", {
+      buffer = args.buf,
+      desc = "Cargo [b]uild",
+    })
+    vim.keymap.set("n", "<leader>cR", "<cmd>LspCargoReload<CR>", {
+      buffer = args.buf,
+      desc = "[R]eload Workspace",
+    })
+  end,
+})
