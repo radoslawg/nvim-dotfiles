67c602e3f5d4301fef1f4eae7aff0eb4be296fd0
Radosław Grzanka <radoslawg@gmail.com>
2026-03-15 21:24:01 +0100

    Change keymap for hints

--- lua/plugins/nvim_lspconfig.lua
+++ lua/plugins/nvim_lspconfig.lua
@@ -108,9 +108,9 @@ return {
         --
         -- This may be unwanted, since they displace some of your code
         if client and client:supports_method("textDocument/inlayHint", event.buf) then
-          map("<leader>th", function()
+          map("<leader>ch", function()
             vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled({ bufnr = event.buf }))
-          end, "[T]oggle Inlay [H]ints")
+          end, "Toggle Inlay [H]ints")
         end
       end,
     })
