94e1ed763ae2c4fe12ccaa5cb2910cd58a95d9c3
Radoslaw Grzanka <radoslawg@gmail.com>
2026-04-26 22:52:38 +0200

    Configure Gitsigns

--- plugin/gitsigns.lua
+++ plugin/gitsigns.lua
@@ -1,2 +1,10 @@
 vim.pack.add({ "https://github.com/lewis6991/gitsigns.nvim" })
-require("gitsigns").setup()
+require('gitsigns').setup {
+  signs = {
+    add = { text = '+' },
+    change = { text = '~' },
+    delete = { text = '_' },
+    topdelete = { text = '‾' },
+    changedelete = { text = '~' },
+  },
+}
